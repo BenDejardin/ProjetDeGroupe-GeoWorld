f4b5c76ac43b42bd4c3fef99ba2cd142d0afe25a
Ben Dejardin <bendejardin8@gmail.com>
2021-03-12 23:33:52 +0100

    Ajout de la déconnexion

--- header.php
+++ header.php
@@ -82,8 +82,12 @@ session_start ();
         </li>
         <?php endif ?>
         <?php if(isset($_SESSION['login'])):?>
-          <li class="nav-item">
-          <a class="nav-link"><?php echo $_SESSION['login'];?></a>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+             aria-expanded="false"><?php echo $_SESSION['login']?></a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item" href="deconnexion.php">Déconnexion</a>
+          </div>
         </li>
         <?php endif ?>
         <li class="nav-item">
